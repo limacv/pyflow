a4dacad62e8d6055dca561e4f7d93c5e2a61eec4
lima <lima@netflixcontractors.com>
2025-04-21 00:16:00 +0000

    allow pip install

--- setup.py
+++ setup.py
@@ -1,24 +1,29 @@
-# Author: Deepak Pathak (c) 2016
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-# from __future__ import unicode_literals
-from distutils.core import setup
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
 from Cython.Build import cythonize
-from distutils.extension import Extension
 from glob import glob
-
 import numpy
 
-sourcefiles = ['pyflow.pyx', ]
+# Define all C++ and Cython source files
+sourcefiles = ['pyflow.pyx']
 sourcefiles.extend(glob("src/*.cpp"))
-extensions = [Extension("pyflow", sourcefiles, include_dirs=[numpy.get_include()])]
+
+extensions = [
+    Extension(
+        "pyflow",
+        sources=sourcefiles,
+        include_dirs=[numpy.get_include(), 'src'],
+        language="c++",
+        extra_compile_args=["-std=c++11"]
+    )
+]
+
 setup(
     name="pyflow",
     version="1.0",
     description="Python wrapper for the Coarse2Fine Optical Flow code.",
     author="Deepak Pathak",
     ext_modules=cythonize(extensions),
-    include_dirs=[numpy.get_include()]
+    zip_safe=False,
+    install_requires=["numpy", "cython"],
 )
